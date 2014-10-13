5e9d0b758ca19255b0bebcb6841c40fc5ea99d4e
Johannes Simon <johannes.simon@mail.de>
2014-10-13 14:12:43 +0200

    - process redirects in a dedicated hadoop job

--- src/main/java/de/tudarmstadt/lt/wiki/hadoop/RedirectReplacer.java
+++ src/main/java/de/tudarmstadt/lt/wiki/hadoop/RedirectReplacer.java
@@ -1,26 +1,61 @@
 package de.tudarmstadt.lt.wiki.hadoop;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import org.apache.hadoop.mapreduce.lib.reduce.IntSumReducer;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 import org.apache.log4j.Logger;
 
-public class HadoopSurfaceFormDictionary extends Configured implements Tool {
-	private static class HadoopSurfaceFormDictionaryMap extends Mapper<LongWritable, Text, Text, IntWritable> {
+import de.tudarmstadt.lt.util.MapUtil;
+import de.tudarmstadt.lt.util.MonitoredFileReader;
+
+public class RedirectReplacer extends Configured implements Tool {
+	private static class RedirectReplacerMap extends Mapper<LongWritable, Text, Text, Text> {
 		Logger log = Logger.getLogger("de.tudarmstadt.lt.wiki");
+		
+		Map<String, String> redirects = new HashMap<>();
+		
+		@Override
+		public void setup(Context context) throws IOException {
+			Configuration conf = context.getConfiguration();
+			FileSystem fs = FileSystem.get(conf);
+			String redirectsFilePattern = conf.get("wiki.redirects.file");
+			if (redirectsFilePattern != null) {
+				log.info("Reading redirects file: " + redirectsFilePattern);
+				try {
+					Path filePath = new Path(redirectsFilePattern);//fileStat.getPath();
+					FileStatus fileStat = fs.getFileStatus(filePath);
+					long fileLen = fileStat.getLen();
+					String fileName = filePath.getName();
+					log.info("Processing redirects file: " + filePath);
+					InputStream in = fs.open(filePath);
+                    BufferedReader reader = new BufferedReader(new MonitoredFileReader(fileName, in, fileLen, "UTF-8", 0.01));
+                    redirects = MapUtil.readMapFromReader(reader, "\t");
+				} catch (Exception e) {
+					log.error("Error reading redirect files", e);
+				}
+			} else {
+				log.error("For redirects to be processed, you need to specify a redirects file"
+						+ "pattern (on HDFS) using -Dwiki.redirects.filepattern=...");
+			}
+		}
 		
 		@Override
 		public void map(LongWritable key, Text value, Context context)
@@ -29,16 +64,17 @@ public class HadoopSurfaceFormDictionary extends Configured implements Tool {
 				String valueParts[] = value.toString().split("\t");
 				String text = valueParts[0];
 				String links[] = valueParts[1].split("  ");
+				LinkedList<String> redirectedLinks = new LinkedList<String>();
 				for (String link : links) {
 					String linkParts[] = link.split("@");
 					String target = linkParts[0];
-					String startEnd[] = linkParts[1].split(":");
-					int start = Integer.parseInt(startEnd[0]);
-					int end = Integer.parseInt(startEnd[1]);
-					String surfaceForm = text.substring(start, end);
-					
-					context.write(new Text(surfaceForm + "@@" + target), new IntWritable(1));
+					String _target = redirects.get(target);
+					if (_target != null) {
+						target = _target;
+					}
+					redirectedLinks.add(target + "@" + linkParts[1]);
 				}
+				context.write(new Text(text), new Text(StringUtils.join(redirectedLinks, "  ")));
 			} catch (Exception e) {
 				log.error("Can't process line: " + value.toString(), e);
 			}
@@ -57,15 +93,14 @@ public class HadoopSurfaceFormDictionary extends Configured implements Tool {
 		conf.setBoolean("mapred.output.compress", true);
 		conf.set("mapred.output.compression.codec", "org.apache.hadoop.io.compress.GzipCodec");
 		Job job = Job.getInstance(conf);
-		job.setJarByClass(HadoopSurfaceFormDictionary.class);
+		job.setJarByClass(RedirectReplacer.class);
 		FileInputFormat.addInputPath(job, new Path(inDir));
 		FileOutputFormat.setOutputPath(job, new Path(_outDir));
-		job.setMapperClass(HadoopSurfaceFormDictionaryMap.class);
-		job.setReducerClass(IntSumReducer.class);
+		job.setMapperClass(RedirectReplacerMap.class);
 		job.setMapOutputKeyClass(Text.class);
-		job.setMapOutputValueClass(IntWritable.class);
+		job.setMapOutputValueClass(Text.class);
 		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(IntWritable.class);
+		job.setOutputValueClass(Text.class);
 //		job.setInputFormatClass(TextInputFormat.class);
 		return job.waitForCompletion(true);
 	}
@@ -84,7 +119,7 @@ public class HadoopSurfaceFormDictionary extends Configured implements Tool {
 
 	public static void main(final String[] args) throws Exception {
 		Configuration conf = new Configuration();
-		int res = ToolRunner.run(conf, new HadoopSurfaceFormDictionary(), args);
+		int res = ToolRunner.run(conf, new RedirectReplacer(), args);
 		System.exit(res);
 	}
 }
